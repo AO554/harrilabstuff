4f4397f1cc1c7db461f52fd143b4cb0c106b7755
Harrison <24486206+AO554@users.noreply.github.com>
2024-03-04 13:05:19 +0000

    3rd party packages and locales

--- 3rd-party/00-rclone.sh
+++ 3rd-party/00-rclone.sh
@@ -6,72 +6,20 @@
 # ██   ██ ██   ██ ██   ██ ██   ██ ██     ██ ███ ██ ██    ██  ███        ██   ██ ██      ██   ██ ██      
 # ██   ██ ██   ██ ██   ██ ██   ██ ██      ███ ███   ██████  ███████     ██   ██ ███████ ██   ██ ███████ 
                                                                                                       
-# Debian Base Packages
-# Last Updated [03-03-2024]
+# rclone Call
+# Last Updated [04-03-2024]
 
-# Check if the script is running as root
+# Check if running as root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-# Configure Global Locales to en_GB.UTF-8
-export LC_ALL=en_GB.UTF-8
-export LANG=en_GB.UTF-8
-export LANGUAGE=en_GB.UTF-8
+# Update Repositories
+apt update
 
-# Generate locales
-sudo locale-gen $LANG
-# Additional locale-gen for US
-sudo locale-gen en_US.UTF-8
-
-# Update and upgrade
-apt update && apt upgrade -y
-
-# Install locales-all first
-apt install -y locales-all
-
-# Set default locale to $LANG
-echo "locales locales/default_environment_locale select $LANG" | sudo debconf-set-selections
-echo "locales locales/locales_to_be_generated multiselect $LANG UTF-8" | sudo debconf-set-selections
-sudo dpkg-reconfigure --frontend=noninteractive locales
-
-# Install base packages
-apt install -y \
-    sudo \
-    curl \
-    wget \
-    nload \
-    vnstat \
-    net-tools \
-    dnsutils \
-    build-essential \
-    fail2ban \
-    gnupg \
-    iptables \
-    python3 \
-    python3-pip \
-    axel \
-    bpytop \
-    git \
-    zsh \
-    neofetch \
-    htop \
-    tmux \
-    tmate \
-    nano \
-    unzip \
-    zip \
-    p7zip-full \
-    bzip2 \
-    ncdu \
-    dialog\
-    ruby
+# Install rclone via script
+curl https://rclone.org/install.sh | bash
 
 # Clean up
-apt autoremove -y
-apt autoclean -y
-apt clean -y
-
-# Done
-echo "Base packages installed successfully"
+exit 0
