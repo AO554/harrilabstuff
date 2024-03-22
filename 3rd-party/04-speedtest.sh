3957962b53fbffe90116fd77261189d2817bd20b
Harrison <24486206+AO554@users.noreply.github.com>
2024-03-22 10:20:10 +0000

    Create 04-speedtest.sh

---
+++ 3rd-party/04-speedtest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# ██   ██  █████  ██████  ██████  ██     ██     ██  ██████  ███████     ██   ██ ███████ ██████  ███████ 
+# ██   ██ ██   ██ ██   ██ ██   ██ ██     ██     ██ ██    ██    ███      ██   ██ ██      ██   ██ ██      
+# ███████ ███████ ██████  ██████  ██     ██  █  ██ ██    ██   ███       ███████ █████   ██████  █████   
+# ██   ██ ██   ██ ██   ██ ██   ██ ██     ██ ███ ██ ██    ██  ███        ██   ██ ██      ██   ██ ██      
+# ██   ██ ██   ██ ██   ██ ██   ██ ██      ███ ███   ██████  ███████     ██   ██ ███████ ██   ██ ███████ 
+                                                                                                      
+# Speedtest Repo
+# Last Updated [21-03-2024]
+
+# Check if running as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Update Repositories
+apt update
+
+# curl
+apt install curl -y
+
+# Install Speedtest
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+apt install speedtest -y
+
+# Done
+exit 0
