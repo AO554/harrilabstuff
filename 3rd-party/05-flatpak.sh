cf38043c9435491b851850ad15abafdbc0fdea3d
Harrison <24486206+AO554@users.noreply.github.com>
2024-05-01 00:42:48 +0100

    As Erlich Bachman once said, MOTHERFUCK-

--- 3rd-party/05-flatpak.sh
+++ 3rd-party/05-flatpak.sh
@@ -6,35 +6,23 @@
 # ██   ██ ██   ██ ██   ██ ██   ██ ██     ██ ███ ██ ██    ██  ███        ██   ██ ██      ██   ██ ██      
 # ██   ██ ██   ██ ██   ██ ██   ██ ██      ███ ███   ██████  ███████     ██   ██ ███████ ██   ██ ███████ 
                                                                                                       
-# Debian fail2ban configuration
-# Last Updated [03-03-2024]
+# Flatpak with Flathub Repo
+# Last Updated [01-05-2024]
 
-# Check if the script is running as root
+# Check if running as root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-# Install fail2ban
-apt install -y fail2ban
+# Update Repositories
+apt update
 
-# Backup the original jail.local file
-cp /etc/fail2ban/jail.local /etc/fail2ban/jail.local.bak
+# Install Flatpak
+apt install flatpak -y
 
-# Set the SSH port to 82
-sed -i 's/port    = ssh/port    = 82/g' /etc/fail2ban/jail.local
-
-# Send the fail2ban Logs to a file in /var/log
-sed -i 's/logpath  = %(sshd_log)s/logpath  = /var/log/fail2ban.log/g' /etc/fail2ban/jail.local
-
-# Touch auth.log
-touch /var/log/auth.log
-
-# Restart the fail2ban service
-systemctl restart fail2ban
-
-# Print the fail2ban status
-systemctl status fail2ban
+# Add Flathub Repo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Done
-echo "fail2ban Configuration Complete"
+exit 0
--- 3rd-party/06-brew.sh
+++ 3rd-party/06-brew.sh
@@ -6,35 +6,21 @@
 # ██   ██ ██   ██ ██   ██ ██   ██ ██     ██ ███ ██ ██    ██  ███        ██   ██ ██      ██   ██ ██      
 # ██   ██ ██   ██ ██   ██ ██   ██ ██      ███ ███   ██████  ███████     ██   ██ ███████ ██   ██ ███████ 
                                                                                                       
-# Debian fail2ban configuration
-# Last Updated [03-03-2024]
+# brew.sh
+# Last Updated [01-05-2024]
 
-# Check if the script is running as root
+# Check if running as root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-# Install fail2ban
-apt install -y fail2ban
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Backup the original jail.local file
-cp /etc/fail2ban/jail.local /etc/fail2ban/jail.local.bak
-
-# Set the SSH port to 82
-sed -i 's/port    = ssh/port    = 82/g' /etc/fail2ban/jail.local
-
-# Send the fail2ban Logs to a file in /var/log
-sed -i 's/logpath  = %(sshd_log)s/logpath  = /var/log/fail2ban.log/g' /etc/fail2ban/jail.local
-
-# Touch auth.log
-touch /var/log/auth.log
-
-# Restart the fail2ban service
-systemctl restart fail2ban
-
-# Print the fail2ban status
-systemctl status fail2ban
+# Fix PATH
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/linuxbrew/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Done
-echo "fail2ban Configuration Complete"
+exit 0
