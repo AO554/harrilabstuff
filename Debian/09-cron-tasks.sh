609b27ea62d5a80f2397ae31287e38f3a0c00af9
AO554 <24486206+AO554@users.noreply.github.com>
2024-07-06 23:02:26 +0100

    Implimenting some new server automation tings
    oh yeah I also put in some mini swap features

--- Debian/09-cron-tasks.sh
+++ Debian/09-cron-tasks.sh
@@ -6,56 +6,43 @@
 # ██   ██ ██   ██ ██   ██ ██   ██ ██     ██ ███ ██ ██    ██  ███        ██   ██ ██      ██   ██ ██      
 # ██   ██ ██   ██ ██   ██ ██   ██ ██      ███ ███   ██████  ███████     ██   ██ ███████ ██   ██ ███████ 
                                                                                                       
-# Debian swapfile installation
-# Last Updated [03-03-2024]
+# Debian Cron Task List
+# Last Updated [05-07-2024]
 
-# Check if the script is running as root
+# Check if running as root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-# Check if `dialog` is installed
-if ! [ -x "$(command -v dialog)" ]; then
-  echo 'dialog is not installed. Installing...' >&2
-  apt-get install -y dialog
-fi
+# Collect list of available tasks for dialog into an array
+scripts=(/opt/harrilabstuff/crontasks/*.sh)
 
-# Dialog for swapfile size (2, 4, 8, 16, 32, 64, 128)
-dialog --title "Swapfile Size" --menu "Choose the size of the swapfile" 10 40 7 \
-256M  "256MB" \
-512M "512MB" \
-1G "1GB" \
-2G "2GB" \
-4G "4GB" \
-8G "8GB" \
-16G "16GB" \
-32G "32GB" \
-64G "64GB" \
-128G "128GB" 2> /tmp/swapfile_size
+# Create a menu items array
+menu_items=()
+for i in "${!tasks[@]}"; do
+  filename=$(basename "${tasks[$i]}")
+  menu_items+=("$i" "$filename")
+done
+
+# Run dialog to give options to run other scripts
+dialog --title "Select a script to run" \
+--menu "Choose one of the following options:" 15 40 4 \
+"${menu_items[@]}" 2> /tmp/selection
 
 # Check the exit status of dialog
 if [ $? -eq 0 ]; then
-    # Get the swapfile size from the dialog
-    SWAPFILE_SIZE=$(cat /tmp/swapfile_size)
-
-    # Create the swapfile
-    fallocate -l $SWAPFILE_SIZE /swapfile
-
-    # Set the correct permissions
-    chmod 600 /swapfile
-
-    # Set up a Linux swap area
-    mkswap /swapfile
-
-    # Enable the swapfile
-    swapon /swapfile
-
-    # Add the swapfile to the /etc/fstab file
-    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+    # Run the selected script
+    bash "${tasks[$(cat /tmp/selection)]}"
 else
     # Exit the script if cancel was pressed
-    echo "Cancel pressed, exiting."
     exit 0
 fi
 
+# return to the previous dialog after the script has finished
+if [ $? -eq 0 ]; then
+    bash /opt/harrilabstuff/Debian/08-3rd-party-pkgs.sh
+fi
+
+# Clean up
+exit 0
---
+++ crontasks/01-automatic-updates.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# ██   ██  █████  ██████  ██████  ██     ██     ██  ██████  ███████     ██   ██ ███████ ██████  ███████ 
+# ██   ██ ██   ██ ██   ██ ██   ██ ██     ██     ██ ██    ██    ███      ██   ██ ██      ██   ██ ██      
+# ███████ ███████ ██████  ██████  ██     ██  █  ██ ██    ██   ███       ███████ █████   ██████  █████   
+# ██   ██ ██   ██ ██   ██ ██   ██ ██     ██ ███ ██ ██    ██  ███        ██   ██ ██      ██   ██ ██      
+# ██   ██ ██   ██ ██   ██ ██   ██ ██      ███ ███   ██████  ███████     ██   ██ ███████ ██   ██ ███████ 
+
+# Automatic Updates Cron Task
+# Last Updated [05-07-2024]
+
+# Check if running as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Add crontask to run automatic updates via apt every Saturday at 1:00 AM
+echo "0 1 * * 6 apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get autoremove -y && apt-get autoclean -y && if [ -f /var/run/reboot-required ]; then echo 'Please reboot your system to apply kernel updates' | mail -s 'Reboot Required' root; fi" > /etc/cron.d/automatic-updates
+
+# Clean up
+exit 0
